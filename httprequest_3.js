5251c23c2783e55cfd46adf9e1ed4db3c41bd3f5
hafeez <ssj3hafeez@hotmail.com>
2019-02-11 21:20:56 +0000

    add step 3

---
+++ httprequest_3.js
@@ -0,0 +1,38 @@
+var https = require('https');  //using require function here//
+
+
+// STEP 3 // 
+
+function getAndPrintHTML(options) {
+console.log(options)
+
+
+
+    // notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+
+   https.get(options,function(response) {
+
+    // creating a string to hold values 
+    let str = ('');
+    // encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when data chunk is rec'd and added 
+    response.on('data', function (data) {
+        str += data;
+    });
+
+
+    //callback is invoked when all of the data has been received
+    response.on('end', function() {
+        console.log(str);
+    });
+   
+});
+}
+var requestOptions = {
+    host: 'themindunleashed.com',
+    path: '/'
+}
+getAndPrintHTML(requestOptions);
